faf1904438a1426a7f61624324929f2eb41b35b1
Viachaslau Kastsechka <viacheslav.kostechko@gmail.com>
2023-03-04 12:18:56 +0100

    project setup

---
+++ ChuckNorris/ChuckNorris/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChuckNorris
+//
+//  Created by Viachaslau Kastsechka on 3/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
